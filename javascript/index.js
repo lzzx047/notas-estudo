c863f853e3984447912c3015f7d4e0f01598b0fe
Luiz <luizmarquesfernando037@gmail.com>
2025-10-30 22:12:55 -0300

    realizando variavel

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,17 @@
 console.log("minha mãe")
+
+// criando variavel 
+// declarando variavel e atribuindo 102 
+// const mediaNotas //CaselCase
+// const mediaNotas // PasquelCase
+// const media_notas //Snacke_Case
+
+let numero = 102
+console.log(numero) 
+numero = 99
+console.log(numero)
+
+const nome = "Luiz"
+console.log(Nome)
+// nome = "Luiz" nao pode atribuir valor
+//  para constante depois de declarar 
