88e3bb9c07c9ed7ea10cfada98e28567519137a1
luiz <luizmarquesfernando037@gmail.com>
2025-11-27 22:15:36 -0300

    aula-exercicio

---
+++ javascript/1-somar.js
@@ -0,0 +1,7 @@
+//c1. Peça ao usuário dois números com casas decimais (números reais),
+// some os dois e mostre o resultado.
+const num = Number(prompt('Digite um numero: '))
+const num2 = Number(prompt('Digite outro numero: '))
+alert("resultado de "+ num + " + " + num2 + " = " + (num + num2))
+
+
---
+++ javascript/13-ParImpar.js
@@ -0,0 +1,7 @@
+const num = Number(prompt('Digite um numero: '))
+const resto= num % 2 
+if(resto == 0){
+    alert("O numero informado é par")
+} else {
+    alert('O numero informado é impar')
+}
